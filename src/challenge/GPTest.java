0aa09bc3a3d989fdd59ff72a5a67a5f5e1ee4452
mmostafa886 <36827060+mmostafa886@users.noreply.github.com>
2020-08-31 12:27:35 +0200

    3

--- src/challenge/GPTest.java
+++ src/challenge/GPTest.java
@@ -1,12 +1,10 @@
 package challenge;
 
-import org.testng.annotations.Test;
-
-import org.testng.annotations.BeforeTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class GPTest {
 
